e3080dd6b1d0ea240069da81fff7798c8b5ea66f
Tristan Ross <tristan.ross@midstall.com>
2022-06-10 18:57:28 -0700

    feat: update webapp and website

--- shell.nix
+++ shell.nix
@@ -1,12 +1,8 @@
 with import <nixpkgs> {};
 
 stdenv.mkDerivation {
-    name = "vista";
+    name = "cerus";
     buildInputs = [
         nodejs-16_x tilt minikube android-udev-rules android-tools python2 kubectl kubernetes-helm
     ];
-    shellHooks = ''
-        export PATH="$PWD/node_modules/.bin/:$PATH"
-        alias run="npm run"
-    '';
 }
